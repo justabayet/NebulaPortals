62d5f20bbcc939b6cd92b6b08ce7db1679987951
justabayet <anthony.bayet@hotmail.com>
2024-02-04 21:30:30 +0100

    + Use hash routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Router } from 'wouter'
+import { useHashLocation } from "wouter/use-hash-location"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Router hook={useHashLocation}>
+      <App />
+    </Router>
   </React.StrictMode>,
 )
