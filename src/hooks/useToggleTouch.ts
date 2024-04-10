dee67c523cb376d22a8984cb2936e38451fb7636
justabayet <anthony.bayet@hotmail.com>
2024-04-10 22:06:38 +0200

    # Improve code readability

--- src/hooks/useToggleTouch.ts
+++ src/hooks/useToggleTouch.ts
@@ -4,27 +4,27 @@ import CameraControls from "camera-controls"
 import { useThree } from "@react-three/fiber"
 
 
-function useToggleTouch(condition: boolean) {
+function useToggleTouch(shouldBeInteractive: boolean) {
   const { controls }: { controls: CameraControls | null } = useThree()
 
   const { events } = useThree()
-  const htmlElement: HTMLElement | null = events.connected
+  const interactiveArea: HTMLElement | null = events.connected
   
-  const isTouch = useIsTouch(htmlElement)
+  const isTouchDevice = useIsTouch(interactiveArea)
 
   useEffect(() => {
-    if (controls == null || htmlElement == null) return
-    if (!isTouch) return
+    if (controls == null || interactiveArea == null) return
+    if (!isTouchDevice) return
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const isConnected = (controls as any)._domElement != null
+    const isInteractive = (controls as any)._domElement != null
 
-    if (condition) {
-      if (!isConnected) controls.connect(htmlElement)
+    if (shouldBeInteractive) {
+      if (!isInteractive) controls.connect(interactiveArea)
     } else {
-      if (isConnected) controls.disconnect()
+      if (isInteractive) controls.disconnect()
     }
-  }, [condition, controls, htmlElement, isTouch])
+  }, [shouldBeInteractive, controls, interactiveArea, isTouchDevice])
 }
 
 export default useToggleTouch
