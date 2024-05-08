6ccd0d58e9073347d891f838a8a7b5c4a121cfcb
justabayet <anthony.bayet@hotmail.com>
2024-05-08 20:16:26 +0200

    # Improve back button

--- src/Overlay/BackButton.tsx
+++ src/Overlay/BackButton.tsx
@@ -1,6 +1,7 @@
 import { useLocation } from 'wouter'
 import './BackButton.css'
 import useCurrentProject from '../hooks/useCurrentProject'
+import backImage from './back.png'
 
 function BackButton(): JSX.Element | undefined {
   const { hasCurrent: isVisible, project } = useCurrentProject()
@@ -17,7 +18,8 @@ function BackButton(): JSX.Element | undefined {
         opacity: isVisible ? 0.8 : 0,
         cursor: isVisible ? 'pointer' : 'auto'
       }}>
-      {'<- Back'}
+      <img src={backImage}
+        style={{ width: '5em' }} />
     </div>
   )
 }
