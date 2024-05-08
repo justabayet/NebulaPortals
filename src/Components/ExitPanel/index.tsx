e0f6e067b450ba72db590dd9313a03491b529262
justabayet <anthony.bayet@hotmail.com>
2024-05-08 20:17:12 +0200

    + ExitPanels are clickable

--- src/Components/ExitPanel/index.tsx
+++ src/Components/ExitPanel/index.tsx
@@ -1,10 +1,11 @@
 // Icon asset from https://www.flaticon.com/free-icons/exit Exit icons created by Freepik - Flaticon
 import { Object3DProps } from '@react-three/fiber'
-import Image from '../Image'
+import { HoverableImage } from '../Image'
 import exitFace from './exit.png'
 import exitLeft from './exit_left.png'
 import exitRight from './exit_right.png'
 import { RoundedBox } from '@react-three/drei/core/RoundedBox'
+import { useRoomData } from '../../provider/RoomDataProvider'
 
 const DEPTH = 0.2
 
@@ -21,10 +22,16 @@ interface ExitPanelProps extends Object3DProps {
 }
 
 function ExitPanel({ direction = 'face', ...props }: ExitPanelProps): JSX.Element {
+  const { name, setLocation } = useRoomData()
+
   return (
     <object3D {...props}>
       <object3D scale={0.5}>
-        <Image src={images[direction]} position={[0, 0, (DEPTH / 2) + 0.001]} radius={0} scale={0.9} isBasicMaterial />
+        <HoverableImage src={images[direction]} position={[0, 0, (DEPTH / 2) + 0.001]} radius={0} scale={0.9} isBasicMaterial
+          onClick={(e) => {
+            e.stopPropagation()
+            setLocation('previous/' + name)
+          }} />
         <mesh>
           <RoundedBox args={[0.95, 0.5, DEPTH]} />
           <meshBasicMaterial color={'white'} />
